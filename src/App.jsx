85c3881a2948a02008650ed913306a7392b98a00
GiordanoCasale <giordanocasale8@gmail.com>
2025-05-21 11:26:14 +0200

    [UPD] Milestone 3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -46,98 +46,106 @@ function App() {
   }
 
   return (
-    <div className="container mt-5">
-      <div className="row mb-4">
-        <div className="col">
+    <div className="container-fluid bg-dark text-light">
+      {/* Header */}
+      <div className="row justify-content-center mb-5">
+        <div className="col-12 col-md-8 col-lg-6">
+          <h1 className="text-center text-primary mb-4">BOOLFLIX</h1>
           <div className="input-group">
             <input
               type="text"
-              className="form-control"
+              className="form-control bg-dark text-light border-primary"
               value={input}
               onChange={(e) => SetInput(e.target.value)}
               placeholder="Cerca un film..."
             />
-            <button className="btn btn-primary" onClick={handleclick}>
+            <button className="btn btn-outline-primary" onClick={handleclick}>
               Cerca
             </button>
           </div>
         </div>
       </div>
 
-      <div className="row row-cols-1 row-cols-md-3 g-4">
+
+      <div className="row g-2">
         {tv.map(series => (
-          <div key={series.id} className="col">
-            <div className="card h-100">
+          <div key={series.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div className="card h-100 bg-dark border-primary text-light hover-shadow"
+              style={{ maxWidth: '250px', margin: '0 auto' }}> {/* ridotto maxWidth */}
               <img
                 src={`https://image.tmdb.org/t/p/w500${series.poster_path}`}
                 className="card-img-top"
+                style={{ height: '250px', objectFit: 'cover' }}
                 alt={series.name}
                 onError={(e) => {
                   e.target.src = 'https://via.placeholder.com/500x750?text=No+Image'
                 }}
               />
-              <div className="card-body">
-                <h5 className="card-title">Titolo: {series.name}</h5>
-                <p className="card-text">Titolo Originale: {series.original_name}</p>
-                <p className="card-text">
-                  Lingua: <ReactCountryFlag
+              <div className="card-body p-2">
+                <h5 className="card-title text-primary fs-6">{series.name}</h5>
+                <p className="card-text text-secondary small">{series.original_name}</p>
+                <p className="card-text d-flex align-items-center gap-2">
+                  <ReactCountryFlag
                     countryCode={getCountryCode(series.original_language)}
                     svg
                     style={{
-                      width: '1em',
-                      height: '1em',
+                      width: '1.5em',
+                      height: '1.5em',
                     }}
                   />
                 </p>
-                <p className="card-text">
-                  <strong>Voto:</strong> {[1, 2, 3, 4, 5].map((number) => {
+                <div className="text-warning">
+                  {[1, 2, 3, 4, 5].map((number) => {
                     const vote = Math.ceil(series.vote_average / 2);
-                    if (number <= vote) {
-                      return <span key={number}><i className="fa-solid fa-star star-color"></i></span>;
-                    } else {
-                      return <span key={number}><i className="fa-regular fa-star star-color"></i></span>;
-                    }
+                    return (
+                      <i key={number}
+                        className={`fa${number <= vote ? 's' : 'r'} fa-star me-1`}>
+                      </i>
+                    );
                   })}
-                </p>
+                </div>
               </div>
             </div>
           </div>
         ))}
 
+
         {movies.map(movie => (
-          <div key={movie.id} className="col">
-            <div className="card h-100">
+          <div key={movie.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div className="card h-100 bg-dark border-primary text-light hover-shadow"
+              style={{ maxWidth: '250px', margin: '0 auto' }}> {/* ridotto maxWidth */}
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 className="card-img-top"
+                style={{ height: '250px', objectFit: 'cover' }}
                 alt={movie.title}
                 onError={(e) => {
                   e.target.src = 'https://via.placeholder.com/500x750?text=No+Image'
                 }}
               />
-              <div className="card-body">
-                <h5 className="card-title">Titolo: {movie.title}</h5>
-                <p className="card-text">Titolo Originale: {movie.original_title}</p>
-                <p className="card-text">
-                  Lingua: <ReactCountryFlag
+              <div className="card-body p-2">
+                <h5 className="card-title text-primary fs-6">{movie.title}</h5>
+                <p className="card-text text-secondary small">{movie.original_title}</p>
+                <p className="card-text d-flex align-items-center gap-2">
+                  <ReactCountryFlag
                     countryCode={getCountryCode(movie.original_language)}
                     svg
                     style={{
-                      width: '1em',
-                      height: '1em',
+                      width: '1.5em',
+                      height: '1.5em',
                     }}
                   />
                 </p>
-                <p className="card-text">
-                  <strong>Voto:</strong> {[1, 2, 3, 4, 5].map((number) => {
+                <div className="text-warning">
+                  {[1, 2, 3, 4, 5].map((number) => {
                     const vote = Math.ceil(movie.vote_average / 2);
-                    if (number <= vote) {
-                      return <span key={number}><i className="fa-solid fa-star star-color"></i></span>;
-                    } else {
-                      return <span key={number}><i className="fa-regular fa-star star-color"></i></span>;
-                    }
+                    return (
+                      <i key={number}
+                        className={`fa${number <= vote ? 's' : 'r'} fa-star me-1`}>
+                      </i>
+                    );
                   })}
-                </p>
+                </div>
               </div>
             </div>
           </div>
@@ -146,6 +154,5 @@ function App() {
     </div>
   )
 }
-
 
 export default App
